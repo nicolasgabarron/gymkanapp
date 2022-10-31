94c532a5151c11af1c66a1c69b5cbdd6561985c1
Nicolás Gabarron Blaya <nicolasgabarroncip@gmail.com>
2022-10-31 16:39:54 +0100

    Implement EntityGraph in 'Voluntario' entity

--- api/src/main/java/com/nicogbdev/gymkanapp/repository/VoluntarioRepository.java
+++ api/src/main/java/com/nicogbdev/gymkanapp/repository/VoluntarioRepository.java
@@ -1,6 +1,9 @@
 package com.nicogbdev.gymkanapp.repository;
 
 import com.nicogbdev.gymkanapp.domain.Voluntario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,9 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface VoluntarioRepository extends JpaRepository<Voluntario, Long>, JpaSpecificationExecutor<Voluntario> {}
+public interface VoluntarioRepository extends JpaRepository<Voluntario, Long>, JpaSpecificationExecutor<Voluntario> {
+
+    @Override
+    @EntityGraph("voluntario-entity-graph")
+    Page<Voluntario> findAll(Specification<Voluntario> spec, Pageable pageable);
+}
