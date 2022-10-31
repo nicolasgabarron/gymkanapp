845d8cf1df11be7a9e8a5f88745c7355bb83d2aa
Nicolás Gabarron Blaya <nicolasgabarroncip@gmail.com>
2022-10-31 16:44:07 +0100

    Ignore warnings from 'Voluntario' entity

--- api/src/main/java/com/nicogbdev/gymkanapp/service/mapper/VoluntarioMapper.java
+++ api/src/main/java/com/nicogbdev/gymkanapp/service/mapper/VoluntarioMapper.java
@@ -11,7 +11,7 @@ import org.mapstruct.*;
 /**
  * Mapper for the entity {@link Voluntario} and its DTO {@link VoluntarioDTO}.
  */
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface VoluntarioMapper extends EntityMapper<VoluntarioDTO, Voluntario> {
     @Mapping(target = "usuarioApp", source = "usuarioApp", qualifiedByName = "userId")
     @Mapping(target = "puntoControl", source = "puntoControl", qualifiedByName = "puntoControlId")
