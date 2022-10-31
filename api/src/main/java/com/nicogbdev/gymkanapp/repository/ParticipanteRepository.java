389239c6c4882236ad708ff47dfc1e4f51929b4a
Nicolás Gabarron Blaya <nicolasgabarroncip@gmail.com>
2022-10-31 16:33:30 +0100

    Implement EntityGraph in 'Participante' entity

--- api/src/main/java/com/nicogbdev/gymkanapp/repository/ParticipanteRepository.java
+++ api/src/main/java/com/nicogbdev/gymkanapp/repository/ParticipanteRepository.java
@@ -1,6 +1,9 @@
 package com.nicogbdev.gymkanapp.repository;
 
 import com.nicogbdev.gymkanapp.domain.Participante;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,9 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface ParticipanteRepository extends JpaRepository<Participante, Long>, JpaSpecificationExecutor<Participante> {}
+public interface ParticipanteRepository extends JpaRepository<Participante, Long>, JpaSpecificationExecutor<Participante> {
+
+    @Override
+    @EntityGraph("participante-entity-graph")
+    Page<Participante> findAll(Specification<Participante> spec, Pageable pageable);
+}
