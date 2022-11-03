726f9c9ec75ad513d276efd16650d2a238e0837b
Nicolás Gabarron Blaya <nicolasgabarroncip@gmail.com>
2022-11-03 12:29:35 +0100

    Add AutoComplete Module to Voluntario Module

--- site/src/main/webapp/app/entities/voluntario/voluntario.module.ts
+++ site/src/main/webapp/app/entities/voluntario/voluntario.module.ts
@@ -1,3 +1,4 @@
+import { AutoCompleteModule } from 'primeng/autocomplete';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'app/shared/shared.module';
 import { VoluntarioComponent } from './list/voluntario.component';
@@ -6,8 +7,9 @@ import { VoluntarioUpdateComponent } from './update/voluntario-update.component'
 import { VoluntarioDeleteDialogComponent } from './delete/voluntario-delete-dialog.component';
 import { VoluntarioRoutingModule } from './route/voluntario-routing.module';
 
+
 @NgModule({
-  imports: [SharedModule, VoluntarioRoutingModule],
+  imports: [SharedModule, VoluntarioRoutingModule, AutoCompleteModule],
   declarations: [VoluntarioComponent, VoluntarioDetailComponent, VoluntarioUpdateComponent, VoluntarioDeleteDialogComponent],
 })
 export class VoluntarioModule {}
