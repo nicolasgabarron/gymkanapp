7a27f57b093bb97e894ba945775bda8326b8a997
Nicolás Gabarron Blaya <nicolasgabarroncip@gmail.com>
2022-11-04 12:37:56 +0100

    Create 'download-pdf' component

---
+++ site/src/main/webapp/app/entities/paso-control/download-pdf/download-pdf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-download-pdf',
+  templateUrl: './download-pdf.component.html',
+  styleUrls: ['./download-pdf.component.scss']
+})
+export class DownloadPdfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
